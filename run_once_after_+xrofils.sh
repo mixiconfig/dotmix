f5089697cc157ab49aff0c7faa95c1e96c163bb5
mixiconfig <riduabre@gmail.com>
2024-06-11 17:30:03 -0400

    Initial commit

---
+++ run_once_after_+xrofils.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chmod +x $HOME/.config/rofi/hyprctl.sh
+
+chmod +x $HOME/.config/rofi/wallpaper.sh
+
+chmod +x $HOME/.config/rofi/wallpaper-engine.sh
+
+chmod +x $HOME/.config/rofi/wallpaper-folder.sh
+
+chmod +x $HOME/.config/rofi/init-wallpaper-engine.sh
