702fe8e275c3d857c13916cec15ea7efccfa0777
mixiconfig <riduabre@gmail.com>
2024-06-11 17:47:10 -0400

    Initial commit

---
+++ run_once_after_+xBinls.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+chmod +x $HOME/.local/share/bin/cpuinfo.sh
+
+chmod +x $HOME/.local/share/bin/gpuinfo.sh
+
+chmod +x $HOME/.local/share/bin/cliphist.sh
+
+chmod +x $HOME/.local/share/bin/gamemode.sh
+
+chmod +x $HOME/.local/share/bin/wallbash.sh
+
+chmod +x $HOME/.local/share/bin/quickapps.sh
+
+chmod +x $HOME/.local/share/bin/windowpin.sh
+
+chmod +x $HOME/.local/share/bin/rofilaunch.sh
+
+chmod +x $HOME/.local/share/bin/rofiselect.sh
+
+chmod +x $HOME/.local/share/bin/screenshot.sh
+
+chmod +x $HOME/.local/share/bin/swwwallkon.sh
+
+chmod +x $HOME/.local/share/bin/testrunner.sh
+
+chmod +x $HOME/.local/share/bin/wallbashqt.sh
+
+chmod +x $HOME/.local/share/bin/swwwallbash.sh
+
+chmod +x $HOME/.local/share/bin/themeselect.sh
+
+chmod +x $HOME/.local/share/bin/themeswitch.sh
+
+chmod +x $HOME/.local/share/bin/wbarconfgen.sh
+
+chmod +x $HOME/.local/share/bin/gamelauncher.sh
+
+chmod +x $HOME/.local/share/bin/logoutlaunch.sh
+
+chmod +x $HOME/.local/share/bin/swwwallcache.sh
+
+chmod +x $HOME/.local/share/bin/swwwallpaper.sh
+
+chmod +x $HOME/.local/share/bin/sysmonlaunch.sh
+
+chmod +x $HOME/.local/share/bin/systemupdate.sh
+
+chmod +x $HOME/.local/share/bin/wallbashcava.sh
+
+chmod +x $HOME/.local/share/bin/wallbashcode.sh
+
+chmod +x $HOME/.local/share/bin/wbarstylegen.sh
+
+chmod +x $HOME/.local/share/bin/batterynotify.sh
+
+chmod +x $HOME/.local/share/bin/dontkillsteam.sh
+
+chmod +x $HOME/.local/share/bin/globalcontrol.sh
+
+chmod +x $HOME/.local/share/bin/keybinds_hint.sh
+
+chmod +x $HOME/.local/share/bin/polkitkdeauth.sh
+
+chmod +x $HOME/.local/share/bin/swwwallselect.sh
+
+chmod +x $HOME/.local/share/bin/volumecontrol.sh
+
+chmod +x $HOME/.local/share/bin/wallbashdunst.sh
+
+chmod +x $HOME/.local/share/bin/keyboardswitch.sh
+
+chmod +x $HOME/.local/share/bin/resetxdgportal.sh
+
+chmod +x $HOME/.local/share/bin/wallbashtoggle.sh
+
+chmod +x $HOME/.local/share/bin/wallbashdiscord.sh
+
+chmod +x $HOME/.local/share/bin/wallbashspotify.sh
+
+chmod +x $HOME/.local/share/bin/brightnesscontrol.sh
