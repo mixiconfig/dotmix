496624364b2ef8f59e7917dd8c89f6962d3fe878
mixiconfig <riduabre@gmail.com>
2024-06-11 17:25:06 -0400

    Initial commit

---
+++ run_once_after_+xSH.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+chmod +x $HOME/.config/waybar/launch.sh
+chmod +x $HOME/.config/waybar/themeswitcher.sh
+chmod +x $HOME/.config/waybar/style/Dark/config.sh
+chmod +x $HOME/.config/waybar/style/Colored/config.sh
+
+chmod +x $HOME/.config/waybar/style/Colorful/config.sh
+
+chmod +x $HOME/.config/waybar/style/Vertical/config.sh
+
+chmod +x $HOME/.config/waybar/style/Rose-Pine/config.sh
+
+chmod +x $HOME/.config/waybar/style/Pywal-Pine/config.sh
+
+chmod +x $HOME/.config/waybar/style/mincolorful/config.sh
+
+chmod +x $HOME/.config/waybar/style/Purple-Pine/config.sh
+
+chmod +x $HOME/.config/waybar/style/Tokyo-night/config.sh
+
+chmod +x $HOME/.config/waybar/style/top-minimal/config.sh
+
+chmod +x $HOME/.config/waybar/style/Colored-Line/config.sh
+
+chmod +x $HOME/.config/waybar/style/Black &
+White/config.sh
+
+chmod +x $HOME/.config/waybar/style/Purple-Pine-Blank/config.sh
+
+chmod +x $HOME/.config/waybar/style/Pywal-Pine-Border/config.sh
+
+chmod +x $HOME/.config/waybar/style/Pywal-colorful-Pine/config.sh
+
+chmod +x $HOME/.config/waybar/style/Pywal-Pine-Border-transparent/config.sh
+
+chmod +x $HOME/.config/waybar/style/Pywal-colorful-Pine-transparent/config.sh
