41310321c08c60805085fa63fdad82582d9c6b78
mixiconfig <riduabre@gmail.com>
2024-06-12 00:16:43 -0400

    Initial commit

--- run_once_install_dependencies.sh
+++ run_once_install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S hyprland waybar rofi dunst kitty gtk2 gtk3 nwg-look neovim thunar zsh sddm parallel hyprlock
+sudo pacman -S hyprland waybar rofi dunst kitty gtk2 gtk3 nwg-look neovim thunar zsh sddm parallel hyprlock notification-daemon
