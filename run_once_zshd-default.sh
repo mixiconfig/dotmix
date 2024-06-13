4ca12bd16859382acf43172997cec874b04c78aa
mixiconfig <riduabre@gmail.com>
2024-06-13 13:01:29 -0400

    Initial commit

--- run_once_zshd-default.sh
+++ run_once_zshd-default.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ ! -d "/usr/bin/zsh" ]; then
-	echo "Adding zsh as default shell"
-	chsh -s /usr/bin/zsh
-else
-	echo ".oh-my-zsh already found, skipping."
-fi
+
+echo "Adding zsh as default shell"
+chsh -s /usr/bin/zsh
