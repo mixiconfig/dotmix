d2ab7e932ca743299cd84ac034c1e8b4e9dbd0cc
mixiconfig <riduabre@gmail.com>
2024-10-26 10:51:52 -0400

    Update run_once_after_+xSH.sh

--- run_once_after_+xSH.sh
+++ run_once_after_+xSH.sh
@@ -34,4 +34,14 @@ chmod +x $HOME/.config/waybar/style/Pywal-colorful-Pine/config.sh
 
 chmod +x $HOME/.config/waybar/style/Pywal-Pine-Border-transparent/config.sh
 
-chmod +x $HOME/.config/waybar/style/Pywal-colorful-Pine-transparent/config.sh
+chmod +x $HOME/.config/waybar/style/Pywal-colorful-Pine-transparent/config.sh 
+chmod +x $HOME/.config/waybar/style/Oglo/config.sh
+chmod +x $HOOME/.config/waybar/style/Modern/config.sh
+chmod +x $HOME/.config/waybar/style/confi/config.sh
+chmod +x $HOME/.config/waybar/style/Blue/config.sh
+chmod +x $HOME/.config/waybar/style/Default/config.sh
+chmod +x $HOME/.config/waybar/style/Gruvminimal/config.sh
+chmod +x $HOME/.config/waybar/style/Mincolorful/config.sh
+chmod +x $HOME/.config/waybar/style/Rectanguel/config.sh
+
+
