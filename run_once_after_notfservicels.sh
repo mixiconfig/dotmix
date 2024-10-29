f4a97634e1199d9bef1028e0a6338ace4f82951a
mixiconfig <riduabre@gmail.com>
2024-10-29 11:58:35 -0400

    Update run_once_after_notfservicels.sh

--- run_once_after_notfservicels.sh
+++ run_once_after_notfservicels.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo touch /usr/share/dbus-1/services/org.freedesktop.Notifications.service
-nvim /usr/share/dbus-1/services/org.freedesktop.Notifications.service
+sudo nvim /usr/share/dbus-1/services/org.freedesktop.Notifications.service
