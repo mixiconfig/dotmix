61a85138cd6fef1fef94ee4c9f242ba35a877cae
mixiconfig <riduabre@gmail.com>
2024-10-29 18:56:45 -0400

    Update run_once_after_sddm-themels.sh

--- run_once_after_sddm-themels.sh
+++ run_once_after_sddm-themels.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo cp -r $HOME/.config/sddm-theme/sddm-astronaut-theme/ /usr/share/sddm/themes/
+sudo cp $HOME/.config/hyde/themes/Tokyo\ Night/wallpapers/mafu_trad_wall.png /usr/share/sddm/themes/sddm-astronaut-theme 
+sudo nvim /usr/lib/sddm/sddm.conf.d/default.conf
