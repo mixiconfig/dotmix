432222f3137423dcf3c969bbdab1d45c1470a5ef
mixiconfig <riduabre@gmail.com>
2025-01-12 11:20:11 -0400

    Update run_once_after_1dependencies.sh

--- run_once_after_1dependencies.sh
+++ run_once_after_1dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo pacman -S hyprland waybar rofi dunst kitty gtk2 gtk3 nwg-look neovim thunar zsh sddm parallel hyprlock notification-daemon swaync ttf-jetbrains-mono ttf-jetbrains-mono-nerd pavucontrol pamixer brightnessctl git base-devel fastfetch exa
-inxi qt5-graphicaleffects qt6-5compat jq
+inxi qt5-graphicaleffects qt6-5compat jq imagemagick
