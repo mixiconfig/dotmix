c66b68130bc86a21a54b12faa1e64181a1ed7936
mixiconfig <riduabre@gmail.com>
2026-04-07 22:39:45 -0400

    Update and rename run_once_after_1dependencies.sh to run_once_after_2dependencies.sh

--- run_once_after_1dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-<<<<<<< Updated upstream
-sudo pacman -S hyprland waybar rofi dunst kitty gtk2 gtk3 nwg-look neovim thunar zsh sddm parallel hyprlock notification-daemon swaync ttf-jetbrains-mono ttf-jetbrains-mono-nerd pavucontrol pamixer brightnessctl git base-devel fastfetch exa
-inxi qt5-graphicaleffects qt6-5compat jq imagemagick
-=======
-sudo pacman -S hyprland waybar rofi dunst kitty gtk2 gtk3 nwg-look neovim thunar zsh sddm parallel hyprlock notification-daemon swaync ttf-jetbrains-mono ttf-jetbrains-mono-nerd pavucontrol pamixer brightnessctl git base-devel fastfetch exa inxi qt5-graphicaleffects qt6-5compat
->>>>>>> Stashed changes
